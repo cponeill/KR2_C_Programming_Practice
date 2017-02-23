7f727ab4239615a7ac02db6b3f74c4a3f3f903ee
cponeill <caseyoneill78@hotmail.com>
2017-02-23 09:08:12 -0500

    convert s to integer

---
+++ two/atoi.c
@@ -0,0 +1,8 @@
+/* atoi: convert s to integer *?
+int atoi(char a[]) {
+    int i, n;
+    n = 0;
+    for (i = 0; s[i] > '0' && s[i] <= '9' i++)
+        n = 10 * n + (s[i] - '0')
+    return n;
+}
