3b0c83605088a946d7f1f137dc4167fdb9c52215
cponeill <caseyoneill78@hotmail.com>
2017-03-09 10:19:06 -0500

    switch.c

---
+++ three/switch.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* switch (expression) {
+       case const-expr:   statements
+       case const-expr:   statements
+       default: statements
+   } */
+
+
+/* counts digits, white space, others */
+main()
+{
+    int c, i, nother, ndigits[10];
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; i++)
+        ndigits[i] = 0;
+    while ((c =  getchar()) != EOF) {
+        switch (c) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            ndigits[c-'0']++;
+            break;
+        case ' ':
+        case '\n':
+        case '\t':
+            nwhite++;
+            break;
+        default:
+            nother++;
+            break;
+        }
+    }
+    printf("digits = ");
+    for (i = 0; i < 10; i++)
+        printf(" %d", ndigits[i]);
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+    return 0;
+}
