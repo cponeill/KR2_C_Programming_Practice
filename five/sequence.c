89f5846c415ec3d6dc6528331857a70c87304c79
cponeill <caseyoneill78@hotmail.com>
2017-03-18 10:29:00 -0400

    sequence.c

---
+++ five/sequence.c
@@ -0,0 +1,9 @@
+/* This is an artificial sequence mean to show how to declare a pointer. */
+
+int x = 1, y = 2, z[10];
+int *ip;        /* ip is a pointer to int */
+ip = &x;        /* ip no points to x */
+y = *ip;        /* y is now 1 */
+*ip = 0;        /* x is now 0 */
+ip = &z[10];    /* ip now points to z[0] */
+
