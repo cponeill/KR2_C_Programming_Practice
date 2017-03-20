48913396059713096f72c3dbe297836dcfc3bd04
cponeill <caseyoneill78@hotmail.com>
2017-03-20 09:39:40 -0400

    fixed and tested swap

--- five/swap.c
+++ five/swap.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /* swap: swap integer x with integer y. */
 void swap(int x, int y)
 {
@@ -5,4 +7,15 @@ void swap(int x, int y)
     temp = x;
     x = y;
     y = temp;
+    printf("Swap!\n");
+    printf("%d\n%d\n", x, y);
+}
+
+int main(void)
+{
+    int x = 3;
+    int y = 4;
+    printf("Normal\n");
+    printf("%d\n%d\n", x, y);
+    swap(x, y);
 }
