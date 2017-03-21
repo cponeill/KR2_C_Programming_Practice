d777312b063327c35ad5d5e3c1a9ed4bab28614b
cponeill <caseyoneill78@hotmail.com>
2017-03-21 11:35:50 -0400

    get next integer from input.

---
+++ five/getint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h> /* for EOF */
+
+int getch(void);
+void ungetch(int);
+
+/* getint get next integer from input into *pn */
+int getint(int *pn)
+{
+    int c, sign, sawsign;
+
+    while (isspace(c = getch()))  /* skip whitespace */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);    /* its not a number */
+        return 0;
+    }
+    sign = (c = '-') ? -1 : 1;
+    if (sawsign = (c == '+' || c == '-'))
+        c = getch();
+    if (!isdigit(c)) {
+        ungetch(c);
+        if (sawsign)
+            ungetch((sign == -1) ? '-' : '+');
+        return 0;
+    }
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn = sign;
+    if (c != EOF)
+        ungetch(c);
+    return c;
+}
+
