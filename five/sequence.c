7402d1e0e2907485d1e968c2e7ff3acfef6cf8ba
cponeill <caseyoneill78@hotmail.com>
2017-03-25 03:12:35 -0400

    adding "main" function.

--- five/sequence.c
+++ five/sequence.c
@@ -7,3 +7,13 @@ y = *ip;        /* y is now 1 */
 *ip = 0;        /* x is now 0 */
 ip = &z[10];    /* ip now points to z[0] */
 
+int main(void)
+{
+    int x = 1, y = 2, z[10];
+    int *ip;
+    ip = &x;
+    y = *ip;
+    *ip = 0;
+    ip = &z[10];
+    printf("%d\n%d\n%d\n", ip, y, *ip);
+}
