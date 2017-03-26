019f20f988bae32d39ae34e3382bd37e384e5d88
cponeill <caseyoneill78@hotmail.com>
2017-03-26 10:23:53 -0400

    adding correct "swap(a, b)" function.

--- five/swap.c
+++ five/swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /* swap: swap integer x with integer y. */
-void swap(int x, int y)
+void swapwrng(int x, int y) /* This is the wrong way to do it. */
 {
     int temp;
     temp = x;
@@ -9,6 +9,15 @@ void swap(int x, int y)
     y = temp;
     printf("Swap!\n");
     printf("%d\n%d\n", x, y);
+}
+
+
+void swap(int *px, int *py) /* This is the correct way to do it */
+{
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
 }
 
 int main(void)
