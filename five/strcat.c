502cfac23181b3e95b06d2dca73a9c68d25d2d53
cponeill <caseyoneill78@hotmail.com>
2017-03-27 10:45:00 -0400

    strcat.c

---
+++ five/strcat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void strcpy(char *s, char *t)
+{
+    /* Fill in code. */
+}
+
+void strcat(char *s, char *t)
+{
+    /* Fill in code. */
+}
+
+int main(void)
+{
+    /* Fill in code. */
+}
+
