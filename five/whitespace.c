baa792e8e73d78dbfa6d04f2e880f9e5b9a4dd3a
cponeill <caseyoneill78@hotmail.com>
2017-03-28 10:40:24 -0400

    my poor attempt at creating a function that finds whitespace.

---
+++ five/whitespace.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* my poor attempt at creating a function that funds white space. */
+void find_whitepace(char n)
+{
+    if (n == ' ' || n == EOF) {
+        ;
+    }
+    else {
+        return n;
+}
