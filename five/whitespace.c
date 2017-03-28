031c4c367f847654c586fa236ef3d3ba5f661ee3
cponeill <caseyoneill78@hotmail.com>
2017-03-28 12:25:17 -0400

    adding boolean number finder.

--- five/whitespace.c
+++ five/whitespace.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
-/* my poor attempt at creating a function that funds white space. */
+/* my poor attempt at creating a function that finds white space. */
 void find_whitepace(char n)
 {
-    if (n == ' ' || n == EOF) {
+    if (n == ' ') {
         ;
     }
     else {
         return n;
+}
+
+
+/* my poor attempt at creating a function that finds a number. */
+void is_number(int n)
+{
+    int n[10];
+    i = '1234567890';
+
+    for (int x = 0; x < 10; x++) {
+        (n[x] != i) ? ; : Null;
+   }
 }
