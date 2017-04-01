d02518e02c7430c58b5e0b33eab058b997b560dc
cponeill <caseyoneill78@hotmail.com>
2017-04-01 10:39:18 -0400

    strcat.c

--- five/strcat.c
+++ five/strcat.c
@@ -16,6 +16,21 @@ void strcat(char *s, char *t)
 
 int main(void)
 {
-    /* Fill in code. */
+    char testbuff[128];
+
+    char *test[] = 
+    {
+      "",
+      "1",
+      "12",
+      "123",
+      "1234"
+    };
+
+    size_t numtests = sizeof test / sizeof test[0];
+    size_t thistest;
+    size_t inner;
+
+    /* Add the rest of the code here. */
 }
 
