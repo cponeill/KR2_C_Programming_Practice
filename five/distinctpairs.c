3e21b0e0968c4398d42b7fb9d3bbb35780fb5dc9
cponeill <caseyoneill78@hotmail.com>
2017-04-09 08:27:07 -0400

    adding more code.

--- five/distinctpairs.c
+++ five/distinctpairs.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 
 /* countPairsWithDiff: count distinct pairs in an array of integers. */
-int countPairsWithDiff(int arr[], int k)
+int countPairsWithDiff(int arr[], int n, int k)
 {
-  int i, j;
-  int count = 0;
+  int count = 0, i;
+  qsort(arr, arr+1);
 
-  for (i = 0; i < sizeof(arr); i++)
-  {
-    for (j = i + 1; j < sizeof(arr); j++)
-      if (arr[i] - arr[j] == k || arr[j] - arr[i] == k)
-        count++;
-  }
+  for (i = 0; i < n-1; i++)
+    if (binarySearch(arr, i+1, n-1, arr[i] + k) != -1)
+      count++;
+
   return count;
 }
 
+
+int binarySearch(int arr[], int high, int low, int x)
+{
+  if (high >= low)
+  {
+    int mid = low + (high-low)/2;
+    if (x == arr[mid])
+        return mid;
+    if (x > arr[mid])
+        return binarySearch(arr, low, (high-1), x);
+    else
+        return binarySearch(arr, (low+1), high, x);
+  }
+  return -1;
+}
+
+
+int main(void)
+{
+  int arr[] = {1, 5, 3, 4, 2};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int k = 3;
+  countPairsWithDiff(arr, n, k);
+  return 0;
+}
