028fc8620caff47ec201fe9dceeb55d6c29d2f9d
cponeill <caseyoneill78@hotmail.com>
2017-04-15 11:29:44 -0400

    chapExamples.c

---
+++ five/chapExamples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* These are just quick code examples from the chapter. */
+
+/* Character Pointers and Functions. */
+
+char *pmessage; /* The pointer is declared. */
+pmessage = "now is the time." /* assigns to 'pmessage' a pointer to the char array. */
+
+
+char amessage[] = "now is the time";  /* This is an array. */
+char *bmessage = "now is the time";   /* This is a pointer. */
