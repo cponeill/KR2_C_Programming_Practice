7423c96724850c1530aea8b7df384a91400d6ae6
cponeill <caseyoneill78@hotmail.com>
2017-04-26 03:13:47 -0400

    seven_one.c

---
+++ seven/seven_one.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+  int (*convcase[2])(int) = {toupper, tolower};
+  int func;
+  int result = EXIT_SUCCESS;
+
+  int ch;
+
+  if (argc > 1)
+  {
+    if(toupper((unsigned char)argv[0][0]) == 'U')
+    {
+      func = 0;
+    }
+    else {
+      func = 1;
+    }
+
+    while((ch = getchar()) != EOF)
+    {
+      ch = (*convcase[func])((unsigned char)ch);
+      putchar(ch);
+    }
+  }
+  else {
+    fprintf(stderr, "Unknown name. Can't decide what to do.\n");
+    result = EXIT_SUCCESS;
+  }
+
+  return result;
+}
